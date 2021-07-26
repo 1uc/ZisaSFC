fff342e3a39fc2cf2a6874a9baf1f87806775b79
1uc <1uc@users.noreply.github.com>
2021-07-26 13:56:46 +0000

    deploy: d671bacc251e727e8834d86c9f3c227541fababb

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cmake_3',['CMake',['../md_cmake.html',1,'']]],
+  ['cmake_20usage_3',['CMake Usage',['../cmake_usage.html',1,'']]],
   ['cmake_2emd_4',['cmake.md',['../cmake_8md.html',1,'']]],
   ['conan_5',['Conan',['../md_conan.html',1,'']]],
   ['conan_2emd_6',['conan.md',['../conan_8md.html',1,'']]],
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['cmake_48',['CMake',['../md_cmake.html',1,'']]],
+  ['cmake_20usage_48',['CMake Usage',['../cmake_usage.html',1,'']]],
   ['conan_49',['Conan',['../md_conan.html',1,'']]],
   ['contributing_50',['Contributing',['../md_contributing.html',1,'']]]
 ];
