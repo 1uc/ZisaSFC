119226c3e8915743a5064e3c221cc4804b3e509a
1uc <1uc@users.noreply.github.com>
2022-09-24 13:15:21 +0000

    deploy: 73c552db439629d07f51ef9093f4cd487a107166

--- namespacezisa.js
+++ namespacezisa.js
@@ -5,5 +5,7 @@ var namespacezisa =
     [ "hilbert_index", "namespacezisa.html#aabdd3611c72a1e3ca32609e8ed560de1", null ],
     [ "hilbert_index", "namespacezisa.html#aa9a4b28803c9b20dd473f228dc07fc22", null ],
     [ "hilbert_index", "namespacezisa.html#a1d6b8b6aae9dca45ce7db9ef55ec554b", null ],
+    [ "hilbert_index", "namespacezisa.html#a7f996cd952e719a2469604b3efa1d795", null ],
+    [ "hilbert_index", "namespacezisa.html#a90879de7fe7760c9f3653b8314166b64", null ],
     [ "hilbert_transform", "namespacezisa.html#af5fd36840ad30eec329950c0029b5cb4", null ]
 ];
--- space__filling__curve_8hpp.js
+++ space__filling__curve_8hpp.js
@@ -7,6 +7,8 @@ var space__filling__curve_8hpp =
     [ "hilbert_index", "space__filling__curve_8hpp.html#a6797aa2b29aedaf823ba991c874062d1", null ],
     [ "hilbert_index", "space__filling__curve_8hpp.html#aa9a4b28803c9b20dd473f228dc07fc22", null ],
     [ "hilbert_index", "space__filling__curve_8hpp.html#a1d6b8b6aae9dca45ce7db9ef55ec554b", null ],
+    [ "hilbert_index", "space__filling__curve_8hpp.html#a7f996cd952e719a2469604b3efa1d795", null ],
+    [ "hilbert_index", "space__filling__curve_8hpp.html#a90879de7fe7760c9f3653b8314166b64", null ],
     [ "hilbert_transform", "space__filling__curve_8hpp.html#af5fd36840ad30eec329950c0029b5cb4", null ],
     [ "hilbert_transform", "space__filling__curve_8hpp.html#af4110ce5ec104434c646e492a5398aab", null ],
     [ "luna_curve", "space__filling__curve_8hpp.html#a80047a59929ee07bcc70394c81a216b9", null ]
